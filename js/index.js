0df9a7952292e8e3b23f0c1d75fc88410e64362b
janhenrikfock <jan.henrik.fock@googlemail.com>
2020-07-22 16:31:59 +0200

    Page display none
    ZZ

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
 "use strict";
 
-console.log('it works');
-console.log('hello world');
-console.log('bam');
+var buttonHome = document.querySelector('.link-home');
+var buttonBookmark = document.querySelector('.link-bookmark');
+var buttonCreate = document.querySelector('.link-create');
+var buttonProfile = document.querySelector('.link-profile');
+var pageHome = document.querySelector('.page-home');
+var pageBookmark = document.querySelector('.page-bookmarks');
+var pageCreate = document.querySelector('.page-create');
+var pageProfile = document.querySelector('.page-profile');
+buttonHome.addEventListener('click', function () {
+  pageHome.classList.remove(d - none);
+  pageBookmark.classList.add(d - none);
+  pageCreate.classList.add(d - none);
+  pageProfile.classList.add(d - none);
+});
+buttonBookmark.addEventListener('click', function () {
+  pageHome.classList.add(d - none);
+  pageBookmark.classList.remove(d - none);
+  pageCreate.classList.add(d - none);
+  pageProfile.classList.add(d - none);
+});
+buttonCreate.addEventListener('click', function () {
+  pageHome.classList.add(d - none);
+  pageBookmark.classList.add(d - none);
+  pageCreate.classList.remove(d - none);
+  pageProfile.classList.add(d - none);
+});
+buttonProfile.addEventListener('click', function () {
+  pageHome.classList.add(d - none);
+  pageBookmark.classList.add(d - none);
+  pageCreate.classList.add(d - none);
+  pageProfile.classList.remove(d - none);
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,35 @@
-console.log('it works')
-console.log('hello world')
-console.log('bam')
+const buttonHome = document.querySelector('.link-home')
+const buttonBookmark = document.querySelector('.link-bookmark')
+const buttonCreate = document.querySelector('.link-create')
+const buttonProfile = document.querySelector('.link-profile')
+
+const pageHome = document.querySelector('.page-home')
+const pageBookmark = document.querySelector('.page-bookmarks')
+const pageCreate = document.querySelector('.page-create')
+const pageProfile = document.querySelector('.page-profile')
+
+buttonHome.addEventListener('click', () => {
+  pageHome.classList.remove(d - none)
+  pageBookmark.classList.add(d - none)
+  pageCreate.classList.add(d - none)
+  pageProfile.classList.add(d - none)
+})
+buttonBookmark.addEventListener('click', () => {
+  pageHome.classList.add(d - none)
+  pageBookmark.classList.remove(d - none)
+  pageCreate.classList.add(d - none)
+  pageProfile.classList.add(d - none)
+})
+
+buttonCreate.addEventListener('click', () => {
+  pageHome.classList.add(d - none)
+  pageBookmark.classList.add(d - none)
+  pageCreate.classList.remove(d - none)
+  pageProfile.classList.add(d - none)
+})
+buttonProfile.addEventListener('click', () => {
+  pageHome.classList.add(d - none)
+  pageBookmark.classList.add(d - none)
+  pageCreate.classList.add(d - none)
+  pageProfile.classList.remove(d - none)
+})
